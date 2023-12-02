57a726b94a5db0a49ead130b1777207d0a4c5853
agentnyrro <qwerty2name@gmail.com>
2023-12-02 17:15:09 +0100

    try to fix program not running

--- ProjectGUI/LoginPanel.cpp
+++ ProjectGUI/LoginPanel.cpp
@@ -13,13 +13,14 @@ LoginPanel::LoginPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, cons
     // Tworzenie panelu z formularzami
     formsPanel = new FormsPanel(this, wxID_ANY, wxPoint(10, 100), wxSize(410, 300));
     formsPanel->SetBackgroundColour(wxColour(0, 255, 0)); // Ustawiony na zielony kolor (0, 255, 0)
-    //formsPanel->Hide()
+    //formsPanel->Hide();
+    Bind(wxEVT_SHOW, &LoginPanel::OnShowPanel, this);
 
 }
 
 void LoginPanel::OnShowPanel(wxShowEvent& event)
 {
-    if (!UserCRUD::isLogged()) {
+    if (!UserCRUD::isLogged() && event.IsShown()) {
         label->Hide();
         formsPanel->Show();
     }
