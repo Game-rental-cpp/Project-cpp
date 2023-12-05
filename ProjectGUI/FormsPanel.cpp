8763022d5d88d688fd35661c3c22daff6e93c195
agentnyrro <qwerty2name@gmail.com>
2023-12-05 18:47:04 +0100

    create fields for user input

--- ProjectGUI/FormsPanel.cpp
+++ ProjectGUI/FormsPanel.cpp
@@ -1,10 +1,15 @@
 // FormsPanel.cpp
 #include "FormsPanel.h"
 #include <wx/wx.h>
+#include <wx/textctrl.h>
 
 FormsPanel::FormsPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size)
     : wxPanel(parent, id, pos, size)
 {
     // Tworzenie etykiety
-    label = new wxStaticText(this, wxID_ANY, "Label FormsPanel", wxPoint(10, 50));
+    loginName = new wxTextCtrl(this, wxID_ANY, "login", wxPoint(10, 20));
+    loginPassword = new wxTextCtrl(this, wxID_ANY, "login pass", wxPoint(10, 60));
+
+    signupName = new wxTextCtrl(this, wxID_ANY, "signup", wxPoint(10, 120));
+    signupPassword = new wxTextCtrl(this, wxID_ANY, "signup pass", wxPoint(10, 160));
 }
--- ProjectGUI/FormsPanel.h
+++ ProjectGUI/FormsPanel.h
@@ -1,11 +1,17 @@
 #pragma once
 // FormsPanel.h
 #include <wx/wx.h>
+
+
 class FormsPanel : public wxPanel
 {
 public:
     FormsPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size);
 
 private:
-    wxStaticText* label;
+    wxTextCtrl* loginName;
+    wxTextCtrl* loginPassword;
+
+    wxTextCtrl* signupName;
+    wxTextCtrl* signupPassword;
 };
