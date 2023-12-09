1fbe107e03ec9dc2275808aea390d2dd3daa305b
agentnyrro <qwerty2name@gmail.com>
2023-12-09 14:41:34 +0100

    add login and signup buttons

--- ProjectGUI/FormsPanel.cpp
+++ ProjectGUI/FormsPanel.cpp
@@ -1,4 +1,4 @@
-// FormsPanel.cpp
+﻿// FormsPanel.cpp
 #include "FormsPanel.h"
 #include <wx/wx.h>
 #include <wx/textctrl.h>
@@ -6,10 +6,31 @@
 FormsPanel::FormsPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size)
     : wxPanel(parent, id, pos, size)
 {
-    // Tworzenie etykiety
-    loginName = new wxTextCtrl(this, wxID_ANY, "login", wxPoint(10, 20));
-    loginPassword = new wxTextCtrl(this, wxID_ANY, "login pass", wxPoint(10, 60));
+    // Tworzenie formularza Zaloguj
+    loginName = new wxTextCtrl(this, wxID_ANY, "login", wxPoint(10, 20), wxSize(200, 20));
+    loginPassword = new wxTextCtrl(this, wxID_ANY, "login pass", wxPoint(10, 50), wxSize(200, 20));
+    loginBtn = new wxButton(this, wxID_ANY, "Zaloguj się", wxPoint(10, 80));
 
-    signupName = new wxTextCtrl(this, wxID_ANY, "signup", wxPoint(10, 120));
-    signupPassword = new wxTextCtrl(this, wxID_ANY, "signup pass", wxPoint(10, 160));
+    // Tworzenie formularza Zarejestruj
+    signupName = new wxTextCtrl(this, wxID_ANY, "signup", wxPoint(10, 130), wxSize(200, 20));
+    signupPassword = new wxTextCtrl(this, wxID_ANY, "signup pass", wxPoint(10, 160), wxSize(200, 20));
+    signupBtn = new wxButton(this, wxID_ANY, "Zarejestruj się", wxPoint(10, 190));
+
+    loginBtn->Bind(wxEVT_BUTTON, &FormsPanel::OnLogin, this);
+    signupBtn->Bind(wxEVT_BUTTON, &FormsPanel::OnSignup, this);
+}
+
+// Funkcja obsługi zdarzenia po naciśnięciu przycisku "Zaloguj się"
+void FormsPanel::OnLogin(wxCommandEvent& event)
+{
+    
+
+    this->Layout();
+}
+
+void FormsPanel::OnSignup(wxCommandEvent& event)
+{
+
+
+    this->Layout();
 }
--- ProjectGUI/FormsPanel.h
+++ ProjectGUI/FormsPanel.h
@@ -7,11 +7,15 @@ class FormsPanel : public wxPanel
 {
 public:
     FormsPanel(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size);
+    void OnLogin(wxCommandEvent& event);
+    void OnSignup(wxCommandEvent& event);
 
 private:
     wxTextCtrl* loginName;
     wxTextCtrl* loginPassword;
+    wxButton* loginBtn;
 
     wxTextCtrl* signupName;
     wxTextCtrl* signupPassword;
+    wxButton* signupBtn;
 };
